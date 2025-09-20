f751123f7d9dc3c9b5645d2c9418fcd4d38ce3c4
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-20 07:58:14 +0700

    correct utf

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tmp=$(mktemp -d)
-iconv -f $(uchardet solution/task2.sh) -t UTF8 solution/task2.sh > $tmp/task2.sh
-iconv -f $(uchardet solution/task3.sh) -t UTF8 solution/task3.sh > $tmp/task3.sh
-iconv -f $(uchardet solution/task4.sh) -t UTF8 solution/task4.sh > $tmp/task4.sh
+iconv -f $(uchardet solution/task2.sh) -t UTF-8 solution/task2.sh > $tmp/task2.sh
+iconv -f $(uchardet solution/task3.sh) -t UTF-8 solution/task3.sh > $tmp/task3.sh
+iconv -f $(uchardet solution/task4.sh) -t UTF-8 solution/task4.sh > $tmp/task4.sh
 mv $tmp/task2.sh $tmp/task3.sh $tmp/task4.sh solution
 rm -r $tmp
