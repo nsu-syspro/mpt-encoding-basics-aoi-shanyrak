57c754cc857b1d062a187975e0b97c9a8a0a0f29
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-20 08:15:54 +0700

    rm added

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/evn bash
 tmp=$(mktemp -d)
 iconv -f $(uchardet solution/task2.sh) -t UTF-8 solution/task2.sh > $tmp/task2.sh
 iconv -f $(uchardet solution/task3.sh) -t UTF-8 solution/task3.sh > $tmp/task3.sh
 iconv -f $(uchardet solution/task4.sh) -t UTF-8 solution/task4.sh > $tmp/task4.sh
+rm solution/task2.sh solution/task3.sh solution/task4.sh
 mv $tmp/task2.sh $tmp/task3.sh $tmp/task4.sh solution
 rm -r $tmp
